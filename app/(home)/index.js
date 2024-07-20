1145675eb1b145e331603f3799430e3f95fb3941
Kanhu Charan <kanhu2551996@gmail.com>
2024-07-20 23:54:05 +0530

    add location fetch

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -1,41 +1,97 @@
-import { Alert, StyleSheet, Text, View } from 'react-native'
+import { Alert, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
-import * as LocationGeoCoding from "expo-location";
+import { Octicons } from '@expo/vector-icons';
 
-const index = () => {
-    const [locationServicesEnabled,setLocationServicesEnabled] = useState(false);
-    const [disPlayCurrentAddress,setDisplayCurrentAddress]=useState("fetching your location...");
+const Index = () => {
+  const [locationServicesEnabled, setLocationServicesEnabled] = useState(false);
+  const [displayCurrentAddress, setDisplayCurrentAddress] = useState('Fetching your location...');
 
-    useEffect(()=>{
-      checkIfLocationEnabled();
-      getCurrentLocation();
-    },[]);
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
 
-    const checkIfLocationEnabled = async()=>{
-      let enabled = await Location.hasServicesEnabledAsync();
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
 
-      if(!enabled){
-        Alert.alert("Location services not enabled","Please enable your location to continue",[{text:"OK"}],{cancelable:false})
-      }else{
-        setLocationServicesEnabled(true)
-      }
+    if (!enabled) {
+      Alert.alert(
+        'Location services not enabled',
+        'Please enable your location to continue',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    } else {
+      setLocationServicesEnabled(true);
+    }
+  };
+
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== 'granted') {
+      Alert.alert(
+        'Permission not granted',
+        'Allow the app to use the location service',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+      return;
     }
 
-    const getCurrentLocation = async()=>{
-      let {status} = await Location.requestBackgroundPermissionsAsync();
+    const location = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.High,
+    });
 
-      if(status !== granted){
-        Alert.alert("Permission is not granted","Allow the app to use the location service",[{text:"OK"}],{cancelable:false})
+    console.log(location);
+
+    let { coords } = location;
+    if (coords) {
+      const { latitude, longitude } = coords;
+
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      if (response.length > 0) {
+        const { name, postalCode, city } = response[0];
+        let address = `${name}, ${postalCode}, ${city}`;
+        setDisplayCurrentAddress(address);
+      } else {
+        setDisplayCurrentAddress('Unable to find address');
       }
     }
+  };
+
+  console.log('My address:', displayCurrentAddress);
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
-  )
-}
+    <ScrollView style={{flex:1,backgroundColor:"#f8f8f8"}}>
+      <View style={{flexDirection:"row",alignItems:"center",gap:12,padding:10}}>
+      <Octicons name="location" size={24} color="#e52850" />
+      <View style={{flex:1}}>
+        <Text style={{fontSize:15,fontWeight:500}}>Deliver To</Text>
+        <Text style={{color:"gray",fontSize:15,marginTop:3}}>{displayCurrentAddress}</Text>
+      </View>
+      <Pressable style={{backgroundColor:"#6cb4ee",width:40,height:40,borderRadius:20,justifyContent:"center",alignItems:"center"}}>
+        <Text>5</Text>
+      </Pressable>
+      </View>
+      <View>
+        
+      </View>
+    </ScrollView>
+  );
+};
 
-export default index
+export default Index;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
